fb3218f32903c87f5f1bf72186ac9fec446f65f6
Niraj Tiwari <tniraj7@gmail.com>
2019-03-03 16:17:42 +0900

    added js. css, img for frontend part added dummy response from routes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const publicDirectory = path.join(__dirname, '../public')
+
+const app = express()
+app.use(express.static(publicDirectory))
+
 const port = 3000;
 
 app.get('/', (req,res) => {
-  res.send('Hello Express')
-})
-
-app.get('/help', (req, res) => {
-  res.send('Help Page')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About Page')
+  res.send('<h1>Weather</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.send(`Today's Weather Report`)
+  res.send({
+    city : 'Tokyo',
+    min_temp: '7',
+    max_temp : '12'
+  })
 })
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
