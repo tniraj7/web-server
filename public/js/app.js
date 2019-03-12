f1e1ade6679f6e5639f70e647b490cc4281112ac
Niraj Tiwari <tniraj7@gmail.com>
2019-03-12 23:52:08 +0900

    added the weather info to the page

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,25 @@ console.log('Client side javascript file is loaded');
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const msg1 = document.querySelector('#message1')
+const msg2 = document.querySelector('#message2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const city = search.value
+
     const url = 'http://localhost:3000/weather?city=' + city
-    fetch(url).then((response) => {
-        response.json().then((error) => {
-            console.log(error)
-        }, (data) => {
-            console.log(data)
+    msg1.textContent = 'Loading weather information ...'
+    msg2.textContent = ''
+    
+    fetch(url).then( (response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                msg1.textContent = data.error
+            } else {
+                msg1.textContent = `City : ${data.city}`
+                msg2.textContent = `Minimmum temperature is : ${data.minTemp}.` + '\n' + ` Maximum temperature is : ${data.maxTemp}.` + '\n' + ` Wind Speed is : ${data.windspeed}.`
+            }
         })
     })
 })
